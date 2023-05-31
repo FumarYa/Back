fc14452a5b0205aa8a1ad8cd79750e63976e836d
Caastan <juancc2607@gmail.com>
2023-05-31 11:57:25 +0200

    subida archivo ruta producto

---
+++ src/routes/producto.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+//Mostrar todos los productos
+
+//Mostrar producto por nombre
+
+//Mostrar producto por marca 
+
+//Mostrar producto por tipo
+
+//Mostrar producto por rango de precio 
+
+//Muestra producto por id 
+
+export default router

79346d657c8f52abaa534a224f68705f7c978c80
Caastan <juancc2607@gmail.com>
2023-05-31 11:43:24 +0200

    Subida archivo rutas venta
    He añadido los metodos que quiero que se muestren en este archivo

---
+++ src/routes/ventas.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+const router = Router();
+
+//Mostrar todas las ventas completadas
+
+//Mostrar las ventas usuario por nombre
+
+//Mostrar ventas usuario por id
+
+//Mostrar ventas de un dia
+
+//Añade una venta
+
+//Borra una venta
+
+//Actuliza estado venta
+
+export default router

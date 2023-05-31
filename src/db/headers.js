5cfd69091e757fee0451a3e94149e5d8a4845cc7
Caastan <juancc2607@gmail.com>
2023-05-31 12:42:25 +0200

    subida archivo headers
    subida de archivo para que las consulta tengan que utilizar un token y sea más seguro

---
+++ src/db/headers.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const options = {
+  hostname: 'www.fumarya.com',
+  port: 80,
+  path: '/api',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': req.params.TOKEN
+  }
+};

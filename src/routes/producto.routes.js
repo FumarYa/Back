d5533c996ca680b07641d70c4c9cb4a7c77df3b3
Caastan <juancc2607@gmail.com>
2023-06-04 20:26:29 +0200

    Primera subida Swagger con productos
    He hecho algunos cambios

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { listaproducto, producto_nombre, marca_nombre, tipo_nombre, rango_precio, producto_id } from "../controllers/indexRoutes.js";
+import { listaproducto, producto_nombre, marca_nombre, tipo_nombre, rango_precio, producto_id, producto_add, producto_delete, producto_update} from "../controllers/indexRoutes.js";
 
 const router = Router();
 
@@ -20,5 +20,14 @@ router.get('/rango/:rango',rango_precio);
 
 //Muestra producto por id 
 router.get('/id/:id',producto_id);
+
+//Añade un producto nuevo (para el admin)
+router.post('/add',producto_add);
+
+//Elimina un producto (para el admin)
+router.delete("/delete/:id", producto_delete)
+
+//Actualiza el producto(para el admin)
+router.put("/update/:id", producto_update)
 
 export default router
